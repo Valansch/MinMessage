789478e5f0a2b8367da7d8bb4b46a665c33cb4ec
Maik Wild <mail@maikwild.de>
2019-10-25 17:51:17 +0200

    Rewrite test_network_messages to use minimal spanning trees

--- test/test_network_messages.py
+++ test/test_network_messages.py
@@ -1,13 +1,11 @@
 
 from network import Network
 import network_factory
-from path import Path
-from traversable_path import TraversablePath
 
 from network_interface import NetworkInterface
 from message import Message
 
-import path_finder
+from minimal_spanning_tree_builder import build_minimal_spanning_tree
 
 random_speed = 624352
 network_size = 100
@@ -21,35 +19,92 @@ def setup_module():
 
 
 def test_send_one_message():
-    simple_network = Network()
-    nodeA = simple_network.create_new_node()
-    nodeB = simple_network.create_new_node()
-    simple_network.connect(nodeA, nodeB)
+    network = Network()
+    node0 = network.create_new_node()
+    node1 = network.create_new_node()
 
-    path = Path()
-    path.append(nodeB)
+    network.connect(node0, node1)
 
-    message = Message(TraversablePath(path), "Content")
-    nodeA.send_message(message)
+    network.inject_network_interfaces()
+    
+    msp = build_minimal_spanning_tree(network, node0)
 
-    assert simple_network.total_messages == 1 and nodeB.messages[0].body =="Content"
+    node0.invoke_broadcast("Content")
+
+    assert network.total_messages == 1 and node1.messages[0].body =="Content"
 
 def test_send_transitive_message():
-    simple_network = Network()
-    nodeA = simple_network.create_new_node()
-    nodeB = simple_network.create_new_node()
-    nodeC = simple_network.create_new_node()
-    simple_network.connect(nodeA, nodeB)
-    simple_network.connect(nodeB, nodeC)
+    network = Network()
+    node0 = network.create_new_node()
+    node1 = network.create_new_node()
+    node2 = network.create_new_node()
+    
+    network.connect(node0, node1)
+    network.connect(node1, node2)
 
-    path = Path()
-    path.append(nodeB)
-    path.append(nodeC)
+    network.inject_network_interfaces()
+    
+    node0.invoke_broadcast("Content")
 
-    message = Message(TraversablePath(path), "Content")
-    nodeA.send_message(message)
+    assert network.total_messages == 2
+    assert node1.messages[0].body =="Content"
+    assert node2.messages[0].body =="Content"
 
-    assert simple_network.total_messages == 2 
-    assert nodeB.messages[0].body =="Content"
-    assert nodeC.messages[0].body =="Content"
+def test_split_message():
+    network = Network()
+    node0 = network.create_new_node()
+    node1 = network.create_new_node()
+    node2 = network.create_new_node()
+    
+    network.connect(node0, node1)
+    network.connect(node0, node2)
 
+    network.inject_network_interfaces()
+
+    node0.invoke_broadcast("Content")
+
+    assert network.total_messages == 2
+    assert node1.messages[0].body =="Content"
+    assert node2.messages[0].body =="Content"
+
+    
+def test_transitive_and_split_message():
+    network = Network()
+    node0 = network.create_new_node()
+    node1 = network.create_new_node()
+    node2 = network.create_new_node()
+    node3 = network.create_new_node()
+    
+    network.connect(node0, node1)
+    network.connect(node1, node2)
+    network.connect(node1, node3)
+
+    network.inject_network_interfaces()
+
+    node0.invoke_broadcast("Content")
+
+    assert network.total_messages == 3
+    assert node1.messages[0].body =="Content"
+    assert node2.messages[0].body =="Content"
+    assert node3.messages[0].body =="Content"
+
+
+def test_split_and_join_message():
+    network = Network()
+    node0 = network.create_new_node()
+    node1 = network.create_new_node()
+    node2 = network.create_new_node()
+    node3 = network.create_new_node()
+    
+    network.connect(node0, node1)
+    network.connect(node1, node2)
+    network.connect(node1, node3)
+
+    network.inject_network_interfaces()
+
+    node0.invoke_broadcast("Content")
+
+    assert network.total_messages == 3
+    assert node1.messages[0].body =="Content"
+    assert node2.messages[0].body =="Content"
+    assert node3.messages[0].body =="Content"
