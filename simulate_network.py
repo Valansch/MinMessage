20b9f407726b41d907b15cda3757f26de58bc602
Maik Wild <mail@maikwild.de>
2019-10-25 22:32:48 +0200

    Added simulate_network.py

---
+++ simulate_network.py
@@ -0,0 +1,26 @@
+import network_factory
+from network import Network
+
+from math import log
+
+import time
+def run_network_simulation(network_size, avg_edges_per_node):
+
+    start = time.time()
+
+    print(f"Creating random network of size {str(network_size)}...")
+    network = network_factory.create_random_network(network_size, avg_edges_per_node)
+
+
+    print("Invoking broadcast...")
+    network.nodes[0].invoke_broadcast("Content")
+
+    print(f"Broadcast finished. Total messages send: {str(network.total_messages)} seconds passed: {str(int(100 * (time.time() - start)) / 100)}s") 
+
+if __name__ == '__main__':
+    run_network_simulation(100, 5)
+    run_network_simulation(500, 5)
+    run_network_simulation(1000, 5)
+    run_network_simulation(5000, 5)
+    run_network_simulation(10000, 5)
+    run_network_simulation(30000, 5)
