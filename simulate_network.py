249d5e33943ea6d5be31120c0851d409b3b329cc
Maik Wild <mail@maikwild.de>
2019-10-26 21:46:41 +0200

    Implement arguments

--- simulate_network.py
+++ simulate_network.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 import time
 
 import network_factory
@@ -5,7 +7,6 @@ import network_factory
 
 def run_network_simulation(network_size, avg_edges_per_node):
 
-    print("========================================================================================")
     start = time.time()
 
     print(f"Creating random network of size {str(network_size)}...")
@@ -14,12 +15,40 @@ def run_network_simulation(network_size, avg_edges_per_node):
     print("Invoking broadcast...")
     network.nodes[0].invoke_broadcast("Content")
 
-    print(f"Broadcast finished. Total messages send: {str(network.total_messages)} seconds passed: {str(int(100 * (time.time() - start)) / 100)}s") 
+    print(
+        f"Broadcast finished. Total messages send: {str(network.total_messages)}" +
+        f"seconds passed: {str(int(100 * (time.time() - start)) / 100)}s"
+    )
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Simulates broadcasting a message with minimum number of messages required"
+    )
+    parser.add_argument(
+        "network_size",
+        metavar="<Network size>",
+        type=int,
+        help="The size of the network to simulate.",
+    )
+    parser.add_argument(
+        "avg_edges_per_node",
+        metavar="<Avg edges per node>",
+        default=8,
+        choices=range(1, 20),
+        type=int,
+        help="The average number of edges per node",
+    )
+    return vars(parser.parse_args())
+
+
+def validate_arguments(arguments):
+    if arguments["network_size"] <= 0:
+        sys.exit(1)
+        print("Network size must be a number greater 0.")
+
 
 if __name__ == "__main__":
-    run_network_simulation(100, 5)
-    run_network_simulation(500, 5)
-    run_network_simulation(1000, 5)
-    run_network_simulation(5000, 5)
-    run_network_simulation(10000, 5)
-    run_network_simulation(30000, 5)
+    arguments = parse_arguments()
+    validate_arguments(arguments)
+    run_network_simulation(arguments["network_size"], arguments["avg_edges_per_node"])
